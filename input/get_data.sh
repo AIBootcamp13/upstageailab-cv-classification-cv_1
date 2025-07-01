ce2874386c2f986712b45584b59cdbf20557c5c0
jiehun.ryu <mahomi3@gmail.com>
2025-07-01 03:47:29 +0000

    feat: wget이 없을 경우 curl로 데이터 다운로드 방식 추가

--- input/get_data.sh
+++ input/get_data.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-wget https://aistages-api-public-prod.s3.amazonaws.com/app/Competitions/000356/data/data.tar.gz
+if command -v wget >/dev/null 2>&1; then
+  wget https://aistages-api-public-prod.s3.amazonaws.com/app/Competitions/000356/data/data.tar.gz
+else
+  curl -O https://aistages-api-public-prod.s3.amazonaws.com/app/Competitions/000356/data/data.tar.gz
+fi
 tar -zxvf data.tar.gz
